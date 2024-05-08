3257e405e2ad213a4b76e18d84cf1d24ee54a7b7
Hasan Ayhan Tekeli <37793310+HasanTekeli@users.noreply.github.com>
2024-05-08 16:51:10 +0300

    3 - 15:14

---
+++ go.mod
@@ -0,0 +1,3 @@
+module nicjtutorial
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"nicjtutorial/handlers"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	ph := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	defer s.Shutdown(timeoutContext)
+
+}
