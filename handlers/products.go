b1c87adeaaa2d76c09064865141378af2df6660e
Hasan Ayhan Tekeli <hasan_tekeli@yahoo.com>
2024-05-09 10:11:32 +0300

    episode 3 end

--- handlers/products.go
+++ handlers/products.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"nicjtutorial/data"
@@ -15,11 +14,23 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	//handle an update
+	
+
+	// catch all others
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p*Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
-	d, err := json.Marshal(lp)
+	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
-	rw.Write(d)
 }
